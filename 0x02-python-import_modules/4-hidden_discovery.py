4ee0e0b2b276b095226f620acf38fa51c523699d
Oussamabk37 <oussamaballouk37@gmail.com>
2024-01-05 15:16:55 -0800

    pyt import

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
     import hidden_4
@@ -8,4 +6,3 @@ if __name__ == "__main__":
     for name in names:
         if name[:2] != "__":
             print(name)
-
